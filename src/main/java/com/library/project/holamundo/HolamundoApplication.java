277a48011125b614ab9cda13b92848a4b2ee233e
Dan <jor.morenos@duocuc.cl>
2026-04-09 22:14:08 -0400

    Proyecto Holamundo Finalizado - Estructura Library

---
+++ src/main/java/com/library/project/holamundo/HolamundoApplication.java
@@ -0,0 +1,13 @@
+package com.library.project.holamundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolamundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolamundoApplication.class, args);
+	}
+
+}
